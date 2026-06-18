b7496c856d07649267e7b06fcf7d3837636b2213
Roginie Almeida dos Santos <164258862+Roginie@users.noreply.github.com>
2026-06-18 20:04:24 -0300

    Corrige CRUD: rota PUT com id, bug Carro->Carros, tratamento 404 e status HTTP corretos

--- main.py
+++ main.py
@@ -1,64 +1,57 @@
-#API -> e um lugar para disponibilizar recursos e ou fucinalidade  uma ponte
-#endpoints
-
-
-
-
-
-
-
+# API REST de carros - disponibiliza recursos via endpoints HTTP (CRUD completo)
 
 from flask import Flask, jsonify, make_response, request
 from bd import Carros
 
-#intancia o modulo Flask na nossa variavel app
-app = Flask ('carros')
+app = Flask('carros')
 
-#primeiro metodo - visualizar os dados (GET)
-#app.route -> define que essa funcao e uma rota para que o flask entender quele metodo precisa ser execultado
+
+# GET - lista todos os carros
 @app.route('/carros', methods=['GET'])
-def GET_carros():
-    return Carros
+def get_carros():
+    return jsonify(Carros)
 
-#app.run(port=5000, host='localhost')
 
-#primeiro metodo parte 2 - Visualizad=r dados pr ID (GET / ID)
+# GET por ID - retorna um carro especifico
 @app.route('/carros/<int:id>', methods=['GET'])
-def get_carros_id(id):
-    for carros in Carros:
-        if carros.get('id') == id:
-            return jsonify(carros)
+def get_carro_id(id):
+    for carro in Carros:
+        if carro.get('id') == id:
+            return jsonify(carro)
+    return make_response(jsonify(mensagem='Carro nao encontrado'), 404)
 
-#app.run(port=5000, host='localhost')
 
-#SEGUNDO METODO - CRIA NOVOS DADOS (POST)
+# POST - cria um novo carro
 @app.route('/carros', methods=['POST'])
-def criar_carros():
-    carro = request.json
+def criar_carro():
+    carro = request.get_json()
     Carros.append(carro)
     return make_response(
-        jsonify(mensagem='Carro cadastrado com sucesso !!!', carro=carro)
-                
+        jsonify(mensagem='Carro cadastrado com sucesso!', carro=carro),
+        201
     )
-#app.run(port=5000, host='localhost')
 
-#TERCEIRO METODO  - EDITAR DADOS (PUT)
-@app.route('/carros', methods=['PUT'])
+
+# PUT - edita um carro existente pelo ID
+@app.route('/carros/<int:id>', methods=['PUT'])
 def editar_carro_id(id):
     carro_alterado = request.get_json()
     for indice, carro in enumerate(Carros):
         if carro.get('id') == id:
-            Carro[indice].update(carro_alterado)
+            Carros[indice].update(carro_alterado)
             return jsonify(Carros[indice])
+    return make_response(jsonify(mensagem='Carro nao encontrado'), 404)
 
-#app.run(port=5000, host='localhost')
-# QUARTO METODO DELETAR DADOS (DELETE)
+
+# DELETE - remove um carro pelo ID
 @app.route('/carros/<int:id>', methods=['DELETE'])
 def excluir_carro(id):
     for indice, carro in enumerate(Carros):
         if carro.get('id') == id:
             del Carros[indice]
-            return jsonify({'mensagem:': 'carro excluido com sucesso !!!'})
+            return jsonify(mensagem='Carro excluido com sucesso!')
+    return make_response(jsonify(mensagem='Carro nao encontrado'), 404)
 
 
-app.run(port=5000, host='localhost')
+if __name__ == '__main__':
+    app.run(port=5000, host='localhost', debug=True)
